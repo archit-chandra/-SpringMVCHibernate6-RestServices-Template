6d850d8a0a15b8528f61b8bc4a10f9921110f17e
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-06-23 16:58:23 +0200

    RestController: StudentRestController added

---
+++ src/main/java/com/archit/application/controllers/rest/StudentRestController.java
@@ -0,0 +1,24 @@
+package com.archit.application.controllers.rest;
+
+import com.archit.application.models.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    // define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Archit", "Chandra"));
+        students.add(new Student("Ankush", "Chandra"));
+        students.add(new Student("Richa", "Patel"));
+        return students;
+    }
+}
