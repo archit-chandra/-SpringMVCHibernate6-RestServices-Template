a22729daf0e8d2b7dbd454ff464b1b36c55f6431
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-06-23 17:12:35 +0200

    Refactoring: using @PostConstruct to load data (more efficient)

--- src/main/java/com/archit/application/controllers/rest/StudentRestController.java
+++ src/main/java/com/archit/application/controllers/rest/StudentRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,13 +13,26 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    /**
+     * @PostConstruct runs only once when this bean is constructed
+     * define @PostConstruct to load student data
+     * This is temporary solution to initialize the test data when
+     * not implemented  and coded to load data from service/database
+     */
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Archit", "Chandra"));
         students.add(new Student("Ankush", "Chandra"));
         students.add(new Student("Richa", "Patel"));
+    }
+
+
+    // define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students; // Jackson will convert this student object to JSON
     }
 }
